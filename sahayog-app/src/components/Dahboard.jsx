58d2ab1b7ad79e488096b179ddfa060e1214135d
ananyatewari <ananyatewari0205@gmail.com>
2024-12-01 20:39:24 +0530

    Fixed dashboard

--- sahayog-app/src/components/Dahboard.jsx
+++ sahayog-app/src/components/Dahboard.jsx
@@ -4,6 +4,8 @@ import PieChart from "./PieChart";
 import CardComponent from "./CardComponent";
 import moment from "moment";
 import { ChevronDown, ChevronUp, Clock } from "lucide-react";
+import { useEffect } from "react";
+import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
 
 const Dashboard = ({ alerts }) => {
   const [viewMode, setViewMode] = useState("scroll");
@@ -12,6 +14,7 @@ const Dashboard = ({ alerts }) => {
   const [selectedType, setSelectedType] = useState("all");
   const [isTypeDropdownOpen, setIsTypeDropdownOpen] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
+
 
   // Get unique alert types
   const alertTypes = useMemo(() => {
@@ -69,6 +72,84 @@ const Dashboard = ({ alerts }) => {
 
   return (
     <div className="p-6">
+
+<section id="AlertOverview">
+        {/* Header */}
+        <div className="flex items-center justify-between mb-2">
+          <h1 className="text-xl font-bold">Alert Overview</h1>
+        </div>
+
+        {/* Main Flex Container for Alert Overview and PieChart */}
+        <div className="flex flex-col md:flex-row">
+          <div className="flex-1 w-[20%]">
+            {/* Alert Overview */}
+            <div className="flex flex-row items-start gap-8 mb-4">
+              <div className="text-red-600 font-bold bg-red-100 p-6 rounded-lg shadow-lg w-64">
+                <span className="flex items-center gap-3">
+                  <div className="bg-red-500 h-2 w-2 rounded-full"></div>
+                  <h2 className="text-lg font-semibold">Critical Alerts</h2>
+                </span>
+                <p className="text-xl"> &nbsp; &nbsp; 2</p>
+              </div>
+              <div className="text-yellow-600 font-bold bg-orange-100 p-6 rounded-lg shadow-lg w-64">
+                <span className="flex gap-3 items-center">
+                  <div className="bg-yellow-500 rounded-full h-2 w-2"></div>
+                  <h2 className="text-lg font-semibold">Moderate Alerts</h2>
+                </span>
+                <p className="text-xl"> &nbsp; &nbsp; 5</p>
+              </div>
+              <div className="text-green-500 font-bold bg-green-100 p-6 rounded-lg shadow-lg w-64">
+                <span className="flex items-center gap-4">
+                  <div className="bg-green-500 rounded-full h-2 w-2"></div>
+                  <h2 className="text-lg font-semibold">Low Alerts</h2>
+                </span>
+                <p className="text-xl"> &nbsp; &nbsp; 2</p>
+              </div>
+            </div>
+
+            
+
+            {/* Recent Alerts */}
+            <div className="recent-alerts mb-4 mt-4">
+              <h1 className="text-xl font-semibold mb-4">Recent Alerts</h1>
+              <div className="marquee">
+                <div className="alerts-container">
+                  {alerts.map((alert, index) => (
+                    <div
+                      className={`alert-item ${
+                        alert.color ? "bg-orange-800" : "bg-blue-600"
+                      }`}
+                      key={index}
+                    >
+                      <p className="location w-[full] overflow-hidden text-ellipsis whitespace-nowrap">
+                        {alert.location?.city} {alert.location?.state}
+                      </p>
+
+                      <p className="">{alert?.type}</p>
+                      <p className="text-xs text-gray-300">
+                        {moment(alert?.timestamp).format("h:mm A")}{" "}
+                        {moment(alert?.timestamp).format("DD/MMM/YYYY")}
+                      </p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* PieChart Section */}
+          <div className="w-[20%]">
+            <div>
+              <PieChart />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <hr />
+
+
+
       <section
         id="latestReport"
         className="min-h-screen bg-gradient-to-br from-blue-50 to-blue-100 py-12 px-4"
